0d95faf76c1459b99916f4f49ee75ed97cf3a654
alaym3 <aam5xa@virginia.edu>
2023-01-05 17:12:58 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='MLOps final project',
+    author='alaym3',
+    license='MIT',
+)
