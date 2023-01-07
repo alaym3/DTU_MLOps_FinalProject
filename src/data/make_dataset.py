ef50cffa95b798d5e90039e21ee0c95fbaee61b9
Siddhi <siddhiyjain@gmail.com>
2023-01-07 20:25:32 +0100

    Making data loading files

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -3,17 +3,32 @@ import click
 import logging
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
+from datasets import load_dataset
+from torchvision import transforms
+import torch
+import os
+
 
 
 @click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
+# @click.argument('input_filepath', type=click.Path(exists=True))
 @click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
+
+def main(output_filepath):
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
     """
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
+
+    dataset_name = "rotten_tomatoes"
+    train = load_dataset(dataset_name, split="train")
+    test = load_dataset(dataset_name, split="test")
+    validation = load_dataset(dataset_name, split="validation")
+
+    torch.save(train, os.path.join(output_filepath, 'train.pt'))
+    torch.save(test, os.path.join(output_filepath, 'test.pt'))
+    torch.save(validation, os.path.join(output_filepath, 'validation.pt'))
 
 
 if __name__ == '__main__':
