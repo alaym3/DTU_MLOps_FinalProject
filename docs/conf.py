b3bd67e5a7cc4520ebb0e9dda285ed3e052eaaaa
alaym3 <aam5xa@virginia.edu>
2023-01-12 09:25:34 +0100

    adjust flake8

--- docs/conf.py
+++ docs/conf.py
@@ -66,7 +66,7 @@ release = "0.1"
 # directories to ignore when looking for source files.
 exclude_patterns = ["_build"]
 
-# The reST default role (used for this markup: `text`) to use for all documents.
+# The reST default role (used for this markup: `text`) to use for all documents
 # default_role = None
 
 # If true, '()' will be appended to :func: etc. cross-reference text.
